f157afa8a5925b7356e5fbbfbad1adbfe430296e
Etienne Pretorius <ettienne_pretorius@yahoo.co.uk>
2023-01-11 00:09:30 +0000

    - ``errno_t`` is not supported on linux.

--- test/test_pthread.c
+++ test/test_pthread.c
@@ -9,10 +9,10 @@
 static void check_mutex_init(void **state) {
     pthread_mutex_t mutex;
     pthread_mutex_init_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EINVAL, EAGAIN, ENOMEM, EPERM
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_mutex_init, error[i]);
         assert_int_equal(pthread_mutex_init(&mutex, NULL), error[i]);
@@ -26,10 +26,10 @@ static void check_mutex_destroy(void **state) {
     pthread_mutex_t mutex;
     assert_int_equal(pthread_mutex_init(&mutex, NULL), 0);
     pthread_mutex_destroy_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EINVAL, EBUSY
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_mutex_destroy, error[i]);
         assert_int_equal(pthread_mutex_destroy(&mutex), error[i]);
@@ -42,10 +42,10 @@ static void check_mutex_lock(void **state) {
     pthread_mutex_t mutex;
     assert_int_equal(pthread_mutex_init(&mutex, NULL), 0);
     pthread_mutex_lock_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EINVAL, EAGAIN, EDEADLK
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_mutex_lock, error[i]);
         assert_int_equal(pthread_mutex_lock(&mutex), error[i]);
@@ -60,10 +60,10 @@ static void check_mutex_trylock(void **state) {
     pthread_mutex_t mutex;
     assert_int_equal(pthread_mutex_init(&mutex, NULL), 0);
     pthread_mutex_lock_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EINVAL, EBUSY, EAGAIN
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_mutex_lock, error[i]);
         assert_int_equal(pthread_mutex_lock(&mutex), error[i]);
@@ -80,10 +80,10 @@ static void check_mutex_unlock(void **state) {
     assert_int_equal(pthread_mutex_init(&mutex, NULL), 0);
     assert_int_equal(pthread_mutex_lock(&mutex), 0);
     pthread_mutex_unlock_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EINVAL, EAGAIN, EPERM
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_mutex_unlock, error[i]);
         assert_int_equal(pthread_mutex_unlock(&mutex), error[i]);
@@ -96,10 +96,10 @@ static void check_mutex_unlock(void **state) {
 static void check_rwlock_init(void **state) {
     pthread_rwlock_t rwlock;
     pthread_rwlock_init_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EAGAIN, ENOMEM, EPERM, EBUSY, EINVAL
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_rwlock_init, error[i]);
         assert_int_equal(pthread_rwlock_init(&rwlock, NULL), error[i]);
@@ -113,10 +113,10 @@ static void check_rwlock_destroy(void **state) {
     pthread_rwlock_t rwlock;
     assert_int_equal(pthread_rwlock_init(&rwlock, NULL), 0);
     pthread_rwlock_destroy_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EBUSY, EINVAL
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_rwlock_destroy, error[i]);
         assert_int_equal(pthread_rwlock_destroy(&rwlock), error[i]);
@@ -129,10 +129,10 @@ static void check_rwlock_rdlock(void **state) {
     pthread_rwlock_t rwlock;
     assert_int_equal(pthread_rwlock_init(&rwlock, NULL), 0);
     pthread_rwlock_rdlock_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EINVAL, EAGAIN, EDEADLK
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_rwlock_rdlock, error[i]);
         assert_int_equal(pthread_rwlock_rdlock(&rwlock), error[i]);
@@ -147,10 +147,10 @@ static void check_rwlock_tryrdlock(void **state) {
     pthread_rwlock_t rwlock;
     assert_int_equal(pthread_rwlock_init(&rwlock, NULL), 0);
     pthread_rwlock_tryrdlock_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EBUSY, EINVAL, EAGAIN
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_rwlock_tryrdlock, error[i]);
         assert_int_equal(pthread_rwlock_tryrdlock(&rwlock), error[i]);
@@ -167,10 +167,10 @@ static void check_rwlock_wrlock(void **state) {
     pthread_rwlock_t rwlock;
     assert_int_equal(pthread_rwlock_init(&rwlock, NULL), 0);
     pthread_rwlock_wrlock_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EINVAL, EDEADLK
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_rwlock_wrlock, error[i]);
         assert_int_equal(pthread_rwlock_wrlock(&rwlock), error[i]);
@@ -185,10 +185,10 @@ static void check_rwlock_trywrlock(void **state) {
     pthread_rwlock_t rwlock;
     assert_int_equal(pthread_rwlock_init(&rwlock, NULL), 0);
     pthread_rwlock_trywrlock_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EBUSY, EINVAL
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_rwlock_trywrlock, error[i]);
         assert_int_equal(pthread_rwlock_trywrlock(&rwlock), error[i]);
@@ -204,10 +204,10 @@ static void check_rwlock_unlock(void **state) {
     assert_int_equal(pthread_rwlock_init(&rwlock, NULL), 0);
     assert_int_equal(pthread_rwlock_rdlock(&rwlock), 0);
     pthread_rwlock_unlock_is_overridden = true;
-    const errno_t error[] = {
+    const int error[] = {
             EINVAL, EPERM
     };
-    const uintmax_t limit = sizeof(error) / sizeof(errno_t);
+    const uintmax_t limit = sizeof(error) / sizeof(int);
     for (uintmax_t i = 0; i < limit; i++) {
         will_return(cmocka_test_pthread_rwlock_unlock, error[i]);
         assert_int_equal(pthread_rwlock_unlock(&rwlock), error[i]);
